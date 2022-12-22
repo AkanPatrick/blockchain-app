8b6f91ae60a5674c2220b2de7f7e328ad949f165
Akaninyene Akpan <89919084+AkanPatrick@users.noreply.github.com>
2022-12-22 07:53:07 +0100

    Add files via upload

---
+++ backend/pubsub.py
@@ -0,0 +1,103 @@
+from email import message
+import time
+
+from pubnub.callbacks import SubscribeCallback
+from pubnub.pnconfiguration import PNConfiguration
+from pubnub.pubnub import PubNub
+
+from backend.blockchain.block import Block
+from backend.wallet.transaction  import Transaction
+
+
+pnconfig = PNConfiguration()
+pnconfig.subscribe_key = 'sub-c-3c4d34c7-126c-4bf0-8d4d-6426edfebcaf'
+pnconfig.publish_key = 'pub-c-ae33f54b-3c2f-4ed0-bb0f-76e946c18736'
+
+
+
+CHANNELS = {
+    'TEST': 'TEST',
+    'BLOCK': 'BLOCK',
+    'TRANSACTION': 'TRANSACTION'
+}
+
+
+class Listener(SubscribeCallback):
+    def __init__(self, blockchain, transaction_pool):
+        self.blockchain = blockchain
+        self.transaction_pool = transaction_pool
+
+
+    def message(self, pubnub, message_object):
+        print(f'\n-- Channel: {message_object.channel} | Message: {message_object.message}')
+
+        if message_object.channel == CHANNELS['BLOCK']:
+            block = Block.from_json(message_object.message)
+            potential_chain = self.blockchain.chain[:]
+            potential_chain.append(block)
+
+
+            try:     
+                self.blockchain.replace_chain(potential_chain)
+                self.transaction_pool.clear_blockchain_transactions(
+                    self.blockchain
+                )
+                print('\n -- Successfully replaced the local chain')
+            except Exception as e:
+                print(f'\n -- Did not replace chain: {e}')
+        elif message_object.channel == CHANNELS['TRANSACTION']:
+            transaction = Transaction.from_json(message_object.message)
+            self.transaction_pool.set_transaction(transaction)
+            print('\n -- Set the new transaction in the transaction pool')
+
+
+
+
+class PubSub():
+    """""
+    Handles the pubish/subscribe layer of the application.
+    Provides communication between the nodes of the blockchain network.
+    """""
+    def __init__(self, blockchain, transaction_pool):
+        self.pubnub = PubNub(pnconfig)
+        self.pubnub.subscribe().channels((CHANNELS.values())).execute()
+        self.pubnub.add_listener(Listener(blockchain, transaction_pool))
+
+
+    def publish(self, channel, message):
+        """""
+        Publish the message object to the channel.
+        """""
+        self.pubnub.publish().channel(channel).message(message ).sync()
+
+
+    def broadcast_block(self, block):
+        """""
+        Broadcast a block object to all nodes.
+        """""
+        self.publish(CHANNELS['BLOCK'], block.to_json())
+
+
+    def broadcast_transaction(self, transaction):
+        """""
+        Broadcast a block object to all nodes.
+        """""
+        self.publish(CHANNELS['TRANSACTION'], transaction.to_json())
+
+   
+    
+
+
+def main():
+    pubsub = PubSub()
+
+    time.sleep(1)
+
+
+    pubsub.publish(CHANNELS['TEST'], { 'foo':  'bar' })
+
+
+if __name__ == '__main__':
+    main()
+
+
